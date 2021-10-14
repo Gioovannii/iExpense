cfbd24f59b304a20c5ab79fd4776f1182dfe4172
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-14 14:12:18 +0800

    new file for coddle

---
+++ iExpense/CodableView.swift
@@ -0,0 +1,20 @@
+//
+//  CodableView.swift
+//  iExpense
+//
+//  Created by Giovanni Gaffé on 2021/10/14.
+//
+
+import SwiftUI
+
+struct CodableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CodableView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodableView()
+    }
+}
