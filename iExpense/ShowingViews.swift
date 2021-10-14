424b9630b88aef403fde5e561bb4d1446a42efb2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-14 11:19:38 +0800

    create second view struct

--- iExpense/ShowingViews.swift
+++ iExpense/ShowingViews.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct SecondView: View {
+    var body: some View {
+        Text("Second view")
+    }
+}
+
 struct ShowingViews: View {
     var body: some View {
         Text("Hello, World!")
