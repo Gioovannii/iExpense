c5e0e5fab15b021d76f81ffbac4c9fc2eb94c584
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-14 11:18:23 +0800

    new file

---
+++ iExpense/ShowingViews.swift
@@ -0,0 +1,20 @@
+//
+//  ShowingViews.swift
+//  iExpense
+//
+//  Created by Giovanni Gaffé on 2021/10/14.
+//
+
+import SwiftUI
+
+struct ShowingViews: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ShowingViews_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowingViews()
+    }
+}
