7baeadef11d2df9e59760dcadbac6c1ba8e1ef47
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-14 10:56:40 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Giovanni Gaffé on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
