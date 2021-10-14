17c6efd34a502ecc59afe72e0d2ac0f1d58b8ca3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-14 11:46:23 +0800

    New file

---
+++ iExpense/DeleteItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  DeleteItemsView.swift
+//  iExpense
+//
+//  Created by Giovanni Gaffé on 2021/10/14.
+//
+
+import SwiftUI
+
+struct DeleteItemsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DeleteItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteItemsView()
+    }
+}
